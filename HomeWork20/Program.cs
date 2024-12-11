6df2f62f44bf9463bff496818e5e76765f40b2b6
e.goncharenko <e.goncharenko@GR-ILSKIY-PC212>
2024-12-11 13:51:50 +0300

    Добавьте файлы проекта.

---
+++ HomeWork20/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+
+
+{
+    delegate double Mydelegate(double r);
+    static void Main(string[] args)
+    {
+        var r = Convert.ToDouble(Console.ReadLine());
+        
+
+
+
+        double LineCircle(double r) => 2 * Math.PI * r;
+
+        double SquareCircle(double r) => Math.PI * Math.Pow(r, 2);
+
+        double VolumeCircle(double r) => (4 * Math.PI * Math.Pow(r, 3)) / 3;
+
+
+        Get(LineCircle, r, "Длина окружности");
+        Get(SquareCircle, r, "Площадь круга");
+        Get(VolumeCircle, r, "Объем шара");
+    }
+            static void Get(Mydelegate del, double r, string message)
+
+                   
+                   { Console.WriteLine($"{message}, {del(r)}"); }
+                   
+}
